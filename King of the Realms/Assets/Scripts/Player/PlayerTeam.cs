4c258fea63571bda3c3772fa788041d7532c7c55
David Estes-Smargiassi <destessmargiassi@gmail.com>
2018-10-30 20:14:05 -0500

    Added teams and modified CaptureHandler to handle teams

---
+++ King of the Realms/Assets/Scripts/Player/PlayerTeam.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTeam : MonoBehaviour {
+
+    public bool freeForAll;
+
+    [SerializeField]
+    private int mTeam;
+
+    static private int sPlayerCountFFA = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (freeForAll)
+        {
+            mTeam = sPlayerCountFFA;
+            sPlayerCountFFA++;
+        }
+    }
+
+    public void SetTeam(int t)
+    {
+        mTeam = t;
+    }
+
+    public int GetTeam()
+    {
+        return mTeam;
+    }
+
+	void Update ()
+    {	
+	}
+}
--- King of the Realms/Assets/Scripts/Zones/CaptureHandler.cs
+++ King of the Realms/Assets/Scripts/Zones/CaptureHandler.cs
@@ -1,30 +1,103 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CaptureHandler : MonoBehaviour {
 
-    [SerializeField] float timeToCapture = 10.0f;
+    // Nested class
+    private class UserInfo
+    {
+        private int mTeamID;
+        private int mPlayerCountInZone;
 
-    private float m_captureProgress = 0.0f;
-    private float m_playerCount = 0;
-    static float m_timeCounter = 0.0f;
-    private bool m_contested = false;
-    private bool m_captured = false;
+        public UserInfo()
+        {
+            mTeamID = -1;
+            mPlayerCountInZone = 0;
+        }
+
+        public UserInfo(int tID)
+        {
+            mTeamID = tID;
+            mPlayerCountInZone = 1; // We initialize when we add one, so skip that step
+        }
+
+        public UserInfo(int tID, int pCnt)
+        {
+            mTeamID = tID;
+            mPlayerCountInZone = pCnt;
+        }
+
+        public int GetTeamID() { return mTeamID; }
+        public void SetTeamID(int tID) { mTeamID = tID; }
+        public int GetPlayerCountInZone() { return mPlayerCountInZone; }
+        public void IncrementPlayerCount() { mPlayerCountInZone++; }
+        public void DecrementPlayerCount()
+        {
+            mPlayerCountInZone--;
+            if (mPlayerCountInZone < 0)
+            {
+                mPlayerCountInZone = 0;
+            }
+        }
+    };
+
+    // Fields
+    [SerializeField]
+    private float timeToCapture = 10.0f;
+
+    [SerializeField]
+    private float mCaptureProgress = 0.0f;
+
+    private bool mCaptured = false;
+    private List<UserInfo> mListOfUsers;
+    private int mZoneTeam = -1;
+
+    static float mTimeCounter = 0.0f;
 
     void Start()
     {
+        mListOfUsers = new List<UserInfo>();
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        m_playerCount++;
+        if(other.tag == "Player")
+        {
+            int playerTeam = other.GetComponent<PlayerTeam>().GetTeam();
+            // check if this user's team exists in the list of Users
+            bool foundInList = false;
+            for (int i = 0; i < mListOfUsers.Count; i++)
+            {
+                if (playerTeam == mListOfUsers[i].GetTeamID())
+                {
+                    mListOfUsers[i].IncrementPlayerCount();
+                    foundInList = true;
+                }
+            }
+            // if not found, add a new entry
+            if(!foundInList)
+            {
+                mListOfUsers.Add(new UserInfo(playerTeam));
+            }
+        }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        m_playerCount--;
+        if (other.tag == "Player")
+        {
+            int teamID = other.GetComponent<PlayerTeam>().GetTeam();
+
+            for (int i = 0; i < mListOfUsers.Count; i++)
+            {
+                if (teamID == mListOfUsers[i].GetTeamID())
+                {
+                    mListOfUsers[i].DecrementPlayerCount();
+                }
+            }
+        }
     }
 
     private void OnTriggerStay(Collider other)
@@ -38,27 +111,47 @@ public class CaptureHandler : MonoBehaviour {
 
     private void HandleCapture(GameObject capturer)
     {
-        if (m_captureProgress < 100.0f)
+        if (mCaptureProgress < 100.0f && !IsZoneContested())
         {
-            m_captureProgress += (0.2f * Time.deltaTime) * 100.0f / timeToCapture;
-            m_timeCounter += 0.5f * Time.deltaTime;
-            CaptureStatusManager.captureStatus = "Capturing..." + m_captureProgress.ToString("F0") + "%";
+            mCaptureProgress += (0.2f * Time.deltaTime) * 100.0f / timeToCapture;
+            mTimeCounter += 0.5f * Time.deltaTime;
+            CaptureStatusManager.captureStatus = "Capturing..." + mCaptureProgress.ToString("F0") + "%";
         }
-        else if (m_captureProgress >= 100.0f)
+        else if (mCaptureProgress >= 100.0f)
         {
-            m_captured = true;
+            mCaptured = true;
+            mZoneTeam = capturer.GetComponent<PlayerTeam>().GetTeam();
             CaptureStatusManager.captureStatus = "Zone Captured";
-            ScoreManager.incrementScore = true;
+            ScoreManager.incrementScore = true; // Needs to be modified to count up for the correct team
         }
     }
 
     private void FixedUpdate()
     {
-        if (m_playerCount == 0 && !m_contested && !m_captured && m_captureProgress >= 0)
+        if (GetNumTeamsInZone() == 0 && !mCaptured && mCaptureProgress >= 0)
         {
-            m_captureProgress -= (0.2f * Time.deltaTime) * 100.0f / timeToCapture;
-            m_timeCounter += 0.5f * Time.deltaTime;
-            CaptureStatusManager.captureStatus = "Capturing..." + m_captureProgress.ToString("F0") + "%";
+            mCaptureProgress -= (0.2f * Time.deltaTime) * 100.0f / timeToCapture;
+            mTimeCounter += 0.5f * Time.deltaTime;
+            CaptureStatusManager.captureStatus = "Capturing..." + mCaptureProgress.ToString("F0") + "%";
         }
+    }
+
+    private bool IsZoneContested()
+    {
+        return (GetNumTeamsInZone() > 1);
+    }
+
+    private int GetNumTeamsInZone()
+    {
+        int numTeamsInZone = 0;
+        for (int i = 0; i < mListOfUsers.Count; i++)
+        {
+            if (mListOfUsers[i].GetPlayerCountInZone() > 0)
+            {
+                numTeamsInZone++;
+            }
+        }
+        return numTeamsInZone;
+
     }
 }
